2415eb96aec1ec2ff1b9126fe5500f43aeb58b34
MushfiQ <2mushfiq@gmail.com>
2021-01-29 00:56:31 -0500

    added message and delete features

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import List
+from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
 
 def home(request):
-    all_items = List.objects.all
-    return render(request, 'home.html', {'all_items': all_items})
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, ('Item has been added'))
+            return render(request, 'home.html', {'all_items': all_items})        
+    else:
+        all_items = List.objects.all
+        return render(request, 'home.html', {'all_items': all_items})
 
 def about(request):
     return render(request, 'about.html', {})
 
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    messages.success(request, ('Item has been Deleted!'))
+    return redirect ('home')
